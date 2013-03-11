9dff7269662f4abdd34fc4022b37f103bfb82bdd
jden <jason@denizac.org>
2013-03-11 04:29:39 -0700

    css fixes for small screens

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var web = require('./web')
 var minq = require('minq')
 minq.verbose = true
 
+process.on('error', error)
+
 console.log('building a treefort...')
 minq.connect(process.env.connectionString || 'mongodb://localhost/treefort')
 
-web(process.env.NODE_ENV === 'production' ? 80 : 8000)
+var port = process.env.NODE_ENV === 'production' ? 80 : 8000 
+web(port)
+console.log('listening on port ', port)
 
 console.log('started in ' + (process.env.NODE_ENV || 'dev'))
+
+function error(e) {
+  console.log('Uncaught error:', e)
+}
