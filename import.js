818a7928806bfbb7f6b70b8c97b99b16354b372a
jden <jason@denizac.org>
2013-03-12 04:04:15 -0700

    add twitter names to data.json

--- import.js
+++ import.js
@@ -11,6 +11,7 @@ var Show = function(raw) {
   return {
     _id: minq.ObjectId(raw.href.substr('/event/'.length, 24)), //minq.ObjectId()
     name: raw.name,
+    tr: raw.tw,
     hashtag: 'tfort'+ht++,
     venue: 'v'+raw.class.substr(3),
     day: raw.date.substr(9),
@@ -38,15 +39,32 @@ function timestamp(obj) {
     'YYYY-MM-DD h:ma z')._d
 }
 
-minq.connect('mongodb://localhost/treefort')
-minq.verbose = true
-rawData.map(Show).forEach(function (show) {
-  minq.collection('shows').insert(show)
-})
+var cs = 'mongodb://localhost/treefort'
 
-minq.collection('users').insert({
-  username: 'jden',
-  bio: 'real life awesome dude',
-  faves: [],
-  friends: []
+minq.verbose = true
+minq.connect(cs)
+.then(function () {
+  minq.collection('users').drop()  
 })
+.then(function () {
+  return minq.collection('shows').drop()
+})
+.then(function () {
+
+  rawData.map(Show).forEach(function (show) {
+    minq.collection('shows').insert(show)
+  })
+
+  minq.collection('users').insert({
+    name: 'jden',
+    bio: 'real life awesome dude',
+    faves: {
+      shows: [],
+      venues: [],
+      users: []
+    }
+  })
+
+}).then(function () {
+  process.exit()
+}).done()
